27919b0893daee5983211ef712076567865f9e8e
José Adán Quintero Sánchez <adanqz@gmail.com>
2023-11-22 18:10:14 -0600

    Add files via upload

--- cam-version.py
+++ cam-version.py
@@ -14,6 +14,8 @@ import cv2
 from pyzbar.pyzbar import decode
 from pyzbar import pyzbar
 import time
+from PIL import ImageTk
+
 
 uri = "mongodb+srv://mongo:Miuniversidad2023@cluster0.sfa5efq.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -25,7 +27,8 @@ class SampleApp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.title_font = tkfont.Font(family='Verdana', size=16)
-        self.geometry("400x950")
+        self.cuerpo_font = tkfont.Font(family='Verdana', size=12)
+        self.geometry("500x950")
         self.title("ParticipoMX")
         self.configure(bg="black")
         # the container is where we'll stack a bunch of frames
@@ -38,12 +41,11 @@ class SampleApp(tk.Tk):
 
 
         self.frames = {}
-        for F in (Inicio, ConfirmarDatos, Votar, Gracias, ReconocimientoFacial):
+        for F in (Portada, Inicio, ConfirmarDatos, Votar, Gracias, ReconocimientoFacial):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             frame.configure(bg="black")
             self.frames[page_name] = frame
-
 
             # put all of the pages in the same location;
             # the one on the top of the stacking order
@@ -51,13 +53,45 @@ class SampleApp(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
 
 
-        self.show_frame("Inicio")
+        self.show_frame("Portada")
 
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
+
+
+
+
+
+class Portada(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        image_path = "ine-bd.jpg"  # Updated image file path
+        image = ImageTk.PhotoImage(file=image_path)  # Ruta de la imagen que deseas mostrar
+
+        label = tk.Label(self, image=image, bg="black")
+        label.image = image  # Mantén una referencia a la imagen para evitar que sea eliminada por el recolector de basura
+        label.pack(side="top")
+
+
+        label = tk.Label(self, foreground="Purple", bg="black", height=3,
+                         text="¡Participas tú! \n¡Ganamos todos!", font=controller.title_font)
+        label.pack(side="top", fill="x")
+
+
+        label = tk.Label(self, foreground="White", bg="black", height=12, text="Ahora tu voto influye en\nel Presupuesto Participativo de tu\ncomunidad. \n\nEl mejoramiento de nuestra \nsociedad \nestá al alcance de todos.\n\nUn voto con sentido,\nun voto con intención", font=controller.cuerpo_font)
+        label.pack(side="top", fill="x")
+
+
+        button1 = tk.Button(self, height=3, width=150, bg="purple", relief="groove", foreground="white", bd=2,
+                            padx=3, pady=3, font="Verdana", text="Comienza escaneando tu INE", command=lambda: controller.show_frame("Inicio"))
+        button1.pack()
+
+
 
 
 class Inicio(tk.Frame):
@@ -68,6 +102,7 @@ class Inicio(tk.Frame):
         label = tk.Label(self, foreground="Purple", bg="black", height=3,
                          text="1. Escanea el código QR \nbidimensional de tu INE", font=controller.title_font)
         label.pack(side="top", fill="x")
+
 
         cam = cv2.VideoCapture(0)
         cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -125,181 +160,148 @@ class ReconocimientoFacial(tk.Frame):
 
 
 
+
 class ConfirmarDatos(tk.Frame):
-
-
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
         label = tk.Label(self, foreground="Purple", bg="black", height=3,
                          text="Confirma tus datos", font=controller.title_font)
         label.pack(side="top", fill="x")
 
-        input_vars = []
+        try:
+            cliente = pymongo.MongoClient(uri, serverSelectionTimeoutMS=5000)
+            baseDatos = cliente["padron"]
+            coleccion = baseDatos["padron1"]
+            
+            documento = coleccion.find_one({"clave_elector": {"$regex": ".*\d.*73.*"}})
 
+            if documento:
+                datos = [
+                    ("ID", documento["_id"]),
+                    ("NOMBRE", documento["nombre"]),
+                    ("APELLIDO PATERNO", documento["apellido_paterno"]),
+                    ("APELLIDO MATERNO", documento["apellido_materno"]),
+                    ("ID", documento["id"]),
+                    ("TELÉFONO", documento["telefono"]),
+                    ("HASH ACTUAL", documento["hash_actual"]),
+                    ("HASH PÚBLICO", documento["hash_publico"]),
+                    ("FIRMA", documento["firma"]),
+                    ("HASH PREVIO", documento["hash_previo"]),
+                    ("VOTADO", documento["votado"]),
+                    ("TIMESTAMP", documento["timestamp"]),
+                    ("CLAVE ELECTOR", documento["clave_elector"]),
+                    ("ENTIDAD", documento["entidad"]),
+                    ("ESTADO", documento["estado"]),
+                    ("SECCIÓN", documento["seccion"])
+                ]
 
-        # Crear 13 inputs de entrada de texto con etiquetas personalizadas
-        etiquetas = ["ID", "NOMBRE", "APELLIDO PATERNO", "APELLIDO MATERNO", "ID", "TELÉFONO", "HASH ACTUAL", "HASH PÚBLICO", "FIRMA", "HASH PREVIO", "VOTADO", "TIMESTAMP", "CLAVE ELECTOR", "ENTIDAD", "ESTADO", "SECCIÓN"]
-        valores_predeterminados = ["", "", "", "", "", "", "", "", "","", "", "", "", "", "", "", "", ""]
+                input_vars = []
 
+                for dato in datos:
+                    label = tk.Label(self, foreground="White", bg="black", text=dato[0])
+                    label.pack()
 
-        for i, etiqueta in enumerate(etiquetas):
-            label = tk.Label(self, foreground="White",
-                             bg="black", text=etiqueta)
-            label.pack()
+                    input_var = tk.StringVar()
+                    input_var.set(dato[1])
 
+                    entry = tk.Entry(self, textvariable=input_var)
+                    entry.pack()
+                    input_vars.append(input_var)
 
-            input_var = tk.StringVar()
-            # Establecer contenido predeterminado
-            input_var.set(valores_predeterminados[i])
-            input_vars.append(input_var)
+                button = tk.Button(self, height=3, width=150, bg="purple", relief="groove", foreground="white", bd=2, padx=3, pady=3,
+                                   font="Verdana", text="Continuar", command=lambda: controller.show_frame("Votar") if check_inputs(input_vars) else None)
+                button.pack()
+            else:
+                print("No se encontraron documentos en la colección")
 
+            cliente.server_info()
+            print("Conexión a MongoDB exitosa")
+            cliente.close()
+        except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
+            print("Tiempo excedido " + str(errorTiempo))
+        except pymongo.errors.ConnectionFailure as errorConexion:
+            print("Fallo al conectarse a MongoDB " + str(errorConexion))
 
-            entry = tk.Entry(self, textvariable=input_var)
-            entry.pack()
-
-
-        def check_inputs():
-            for var in input_vars:
-                if len(var.get()) == 0:
-                    return False
-            return True
-
-
-        button = tk.Button(self, height=3, width=150, bg="purple", relief="groove", foreground="white", bd=2, padx=3, pady=3,
-                           font="Verdana", text="Continuar", command=lambda: controller.show_frame("Votar") if check_inputs() else None)
-        button.pack()
-
-
-
-
-
-def mostrarDatos(tabla, limit=1):
-    try:
-        cliente = pymongo.MongoClient(uri, serverSelectionTimeoutMS=5000)
-        baseDatos = cliente["padron"]
-        coleccion = baseDatos["padron1"]
-
-        documentos = coleccion.find().limit(limit)
-
-        for documento in documentos:
-            datos = [
-                documento["_id"],
-                documento["nombre"],
-                documento["apellido_paterno"],
-                documento["apellido_materno"],
-                documento["id"],
-                documento["telefono"],
-                documento["hash_actual"],
-                documento["hash_publico"],
-                documento["firma"],
-                documento["hash_previo"],
-                documento["votado"],
-                documento["timestamp"],
-                documento["clave_elector"],
-                documento["entidad"],
-                documento["estado"],
-                documento["seccion"]
-            ]
-            tabla.insert('', 'end', values=datos)
-
-        cliente.server_info()
-        print("Consulta con la base de datos de Mongo exitosa")
-        cliente.close()
-    except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
-        print("Tiempo excedido " + str(errorTiempo))
-    except pymongo.errors.ConnectionFailure as errorConexion:
-        print("Fallo al conectarse a MongoDB " + str(errorConexion))
-
-
-
-tabla = ttk.Treeview()
-tabla.pack(fill="both", expand=True)
-tabla["columns"] = ("ID", "NOMBRE", "APELLIDO PATERNO", "APELLIDO MATERNO", "ID", "TELÉFONO", "HASH ACTUAL", "HASH PÚBLICO", "FIRMA", "HASH PREVIO", "VOTADO", "TIMESTAMP", "CLAVE ELECTOR", "ENTIDAD", "ESTADO", "SECCIÓN")
-tabla.column("#0", width=50)
-tabla.column("ID", width=100)
-tabla.column("NOMBRE", width=100)
-tabla.column("APELLIDO PATERNO", width=100)
-tabla.column("APELLIDO MATERNO", width=100)
-tabla.column("ID", width=50)
-tabla.column("TELÉFONO", width=100)
-tabla.column("HASH ACTUAL", width=100)
-tabla.column("HASH PÚBLICO", width=100)
-tabla.column("FIRMA", width=100)
-tabla.column("HASH PREVIO", width=100)
-tabla.column("VOTADO", width=50)
-tabla.column("TIMESTAMP", width=100)
-tabla.column("CLAVE ELECTOR", width=100)
-tabla.column("ENTIDAD", width=100)
-tabla.column("ESTADO", width=100)
-tabla.column("SECCIÓN", width=100)
-tabla.heading("#0", text="ID")
-tabla.heading("ID", text="ID")
-tabla.heading("NOMBRE", text="NOMBRE")
-tabla.heading("APELLIDO PATERNO", text="APELLIDO PATERNO")
-tabla.heading("APELLIDO MATERNO", text="APELLIDO MATERNO")
-tabla.heading("ID", text="ID")
-tabla.heading("TELÉFONO", text="TELÉFONO")
-tabla.heading("HASH ACTUAL", text="HASH ACTUAL")
-tabla.heading("HASH PÚBLICO", text="HASH PÚBLICO")
-tabla.heading("FIRMA", text="FIRMA")
-tabla.heading("HASH PREVIO", text="HASH PREVIO")
-tabla.heading("VOTADO", text="VOTADO")
-tabla.heading("TIMESTAMP", text="TIMESTAMP")
-tabla.heading("CLAVE ELECTOR", text="CLAVE ELECTOR")
-tabla.heading("ENTIDAD", text="ENTIDAD")
-tabla.heading("ESTADO", text="ESTADO")
-tabla.heading("SECCIÓN", text="SECCIÓN")
-
-mostrarDatos(tabla)  # Limit the number of documents to 5
-
+def check_inputs(input_vars):
+    # Verificar si todos los campos de entrada tienen valores no vacíos
+    for var in input_vars:
+        if var.get() == "":
+            return False
+    return True
 
 
 
 
 class Votar(tk.Frame):
-
-
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.configure(bg="black")
-        label = tk.Label(self, foreground="Purple", bg="black", height=3,
-                         text="Elegir Candidato", font=controller.title_font)
+        label = tk.Label(self, foreground="Purple", bg="black", height=3, text="Elegir Candidato", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-
 
         radio_var = tk.StringVar()
 
-
         opciones = {
-            "Claudia Sheinbaum": "id1",
-            "Xochitl Gálvez": "id2",
-            "Adán Augusto López": "id3",
-            "Samuel García": "id4"
+            "Claudia Sheinbaum": "1",
+            "Xochitl Gálvez": "2",
+            "Adán Augusto López": "3",
+            "Samuel García": "4"
         }
-
 
         for opcion, identificador in opciones.items():
             radio_button = tk.Radiobutton(
-                self, foreground="White", bg="black", text=opcion, variable=radio_var, value=identificador)
+                self,
+                foreground="White",
+                bg="black",
+                text=opcion,
+                variable=radio_var,
+                value=identificador
+            )
             radio_button.pack(side="top", padx=25, pady=25)
 
-
-        def check_inputs():
-            selected_option = radio_var.get()
-            if selected_option == "":
-                return False
-            else:
-                print("Voto registrado al ID del candidato:", selected_option)
-                return True
-
-
-        button = tk.Button(self, height=3, width=150, bg="purple", relief="groove", foreground="white", bd=2, padx=3, pady=3,
-                           font="Verdana", text="Enviar voto", command=lambda: controller.show_frame("Gracias") if check_inputs() else None)
+        button = tk.Button(
+            self,
+            height=3,
+            width=150,
+            bg="purple",
+            relief="groove",
+            foreground="white",
+            bd=2,
+            padx=3,
+            pady=3,
+            font="Verdana",
+            text="Continuar",
+            command=lambda: self.registrar_voto(radio_var.get(), controller)
+        )
         button.pack()
 
+    def registrar_voto(self, candidato_id, controller):
+        if candidato_id != "":
+            try:
+                cliente = pymongo.MongoClient(uri, serverSelectionTimeoutMS=5000)
+                baseDatos = cliente["padron"]
+                coleccion = baseDatos["padron1"]
 
+                coleccion.find_one_and_replace({"clave_elector": {"$regex": ".*\d.*73.*"}},{"votado": candidato_id})
+
+                cliente.server_info()
+                print("Voto registrado al ID del candidato:", candidato_id)
+                print("Actualización exitosa")
+                cliente.close()
+
+                controller.show_frame("Gracias")  # Redirigir al frame "Gracias"
+
+            except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
+                print("Tiempo excedido " + str(errorTiempo))
+            except pymongo.errors.ConnectionFailure as errorConexion:
+                print("Fallo al conectarse a MongoDB " + str(errorConexion))
+        else:
+            print("No se seleccionó ningún candidato")
+
+                
 
 
 class Gracias(tk.Frame):
